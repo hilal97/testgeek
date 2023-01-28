7a8066d1e8730a9b2b4feaaee00507dc7602f2a5
hilal97 <71225058+hilal97@users.noreply.github.com>
2023-01-28 12:07:04 +0530

    Jupyter Notebook on IBM Watson Studio
    This is the final assignment of Tools for data science.

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# __Hilaloudine Azeez__ 
+# I would like to become a petroleum engineer.
+
+# *I am interested in data science because nowadays to become a succesful petroleum engineer, you need an additional skill. 
+# And data science is the perfect skill to have at the moment*.
+
+# ### The below code should print out the result 5+8.
+
+# In[3]:
+
+
+print (5+8)
+
+
+# > The rselut of the above cell should be 13.
+# 
+# ******
+# 
+# My personal website is [Hilal Azeez](https://hilalazeez.com)
